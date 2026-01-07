348dd71b34c32f4cd658b0ca786506dde5aa8edf
himanushi <himanushi@users.noreply.github.com>
2026-01-07 22:20:17 +0900

    fix: mod.rs

--- src/hardware/mod.rs
+++ src/hardware/mod.rs
@@ -5,8 +5,10 @@
 pub mod traits;
 pub mod power;
 pub mod io_expander;
+pub mod ambient_light;
 pub mod board;
 
 pub use power::Axp2101;
 pub use io_expander::Aw9523;
-pub use board::Board;
+pub use ambient_light::Ltr553;
+pub use board::{Board, BoardRuntime};
