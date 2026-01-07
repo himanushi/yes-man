04138c1cfc1d75fca85b1acda2b36ee416fd5761
himanushi <himanushi@users.noreply.github.com>
2026-01-07 22:20:16 +0900

    fix: mod.rs

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,5 +3,7 @@
 //! This module contains the main application logic.
 
 pub mod yes_man;
+pub mod scheduler;
 
 pub use yes_man::YesManApp;
+pub use scheduler::{AppScheduler, BacklightController, PeriodicTask};
